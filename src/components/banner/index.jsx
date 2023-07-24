faebbd07444ea6e1047591bef541422192ebfa6a
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-07-24 23:19:52 +0600

    second push

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import c from './banner.module.scss'
+import AOS from 'aos'
 
 const Banner = () => {
+  AOS.init()
   return (
     <div className={c.banner}>
-      <h1>
-        <span>Мы команда разработчиков</span> <br />
-        которые делают свои работы <br />
-        с удовольствием
-      </h1>
+      <div className={c.text}>
+        <div className={c.up}>
+          <h1
+            data-aos="fade-up"
+          >
+            Мы команда разработчиков
+          </h1>
+        </div>
+        <div>
+          <h1
+            data-aos="fade-up"
+          >
+            которые делают свои работы
+          </h1>
+        </div>
+        <div>
+          <h1
+            data-aos="fade-up"
+          >
+            с удовольствием
+          </h1>
+        </div>
+      </div>
       <p>
         Работая с нами получите хорошое удовольствие и качественный результат!
       </p>
